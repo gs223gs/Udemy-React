b8b263f694373d39085db563f4bc0fb35ce5b3b7
T.MiuraPro <mtaiga0213@icloud.com>
2025-02-07 23:56:40 +0900

    mapの使い方 keyを指定すること => 再レンダリングのため keyにはindexは使わない => 配列に追加された時に全て再レンダリングされる

--- react-guide-material/06_control_and_form/src/010_list_components/start/Example.jsx
+++ react-guide-material/06_control_and_form/src/010_list_components/start/Example.jsx
@@ -1,5 +1,13 @@
+const animals = [
+  {key:10, animal:"Dog"}, 
+  {key:20, animal:"Cat"}, 
+  {key:30, animal:"Rat"}
+];
 
-const animals = ["Dog", "Cat", "Rat"];
+// 重複チェック
+const keys = animals.map(animal => animal.key);
+const hasDuplicates = keys.length !== new Set(keys).size;
+console.log("重複があるか:", hasDuplicates);
 
 const Example = () => {
 
@@ -7,7 +15,7 @@ const Example = () => {
     <>
       <h3>配列の操作</h3>
       <ul>
-        
+        {animals.map((animal) => <li key={animal.key}>{animal.animal}</li>)}
       </ul>
     </>
   );
