939d098a54356026139c018b05a498bf4c3491ba
T.MiuraPro <mtaiga0213@icloud.com>
2025-02-07 17:30:54 +0900

    useStateの簡単な使い方とスプレッド演算子

--- react-guide-material/05_state_and_event/src/060_state_object/start/Example.jsx
+++ react-guide-material/05_state_and_event/src/060_state_object/start/Example.jsx
@@ -2,6 +2,20 @@ import { useState } from "react";
 
 const Example = () => {
   const personObj = { name: "Tom", age: 18 };
+  const [person, setPerson] = useState(personObj);
+  return (
+    <div>
+      <p>Name:{person.name}</p>
+      <p>Age: {person.age}</p>
+      <input type="text"
+        onChange={(e) => setPerson({name: e.target.value, age: person.age})}
+      /><br />
+      <input type="number"
+        onChange={(e) => setPerson({...person, age: e.target.value})}
+      /><br />
+      <button onClick={() => setPerson({...person, name: '', age: ''})}>リセット</button>
+    </div>
+  );
 };
 
 export default Example;
