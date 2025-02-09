f4ea023f72890ccd5899a085b14df755d37078ab
T.MiuraPro <mtaiga0213@icloud.com>
2025-02-09 13:32:42 +0900

    chakra-uiの使い方/紹介
    これはすごい．
    今まで何やっていたんだ．生のCSSにはもう戻れない.

--- react-guide-material/07_styling_component/src/050_chakra_ui/start/components/List.jsx
+++ react-guide-material/07_styling_component/src/050_chakra_ui/start/components/List.jsx
@@ -1,9 +1,13 @@
+import { VStack, StackDivider } from "@chakra-ui/react";
+
 const List = ({todos, deleteTodo}) => {
     const complete = (id) => {
         deleteTodo(id)
     }
     return (
-        <div>
+        <VStack
+            divider={<StackDivider borderColor={"gray.300"} />}
+        >
             {todos.map(todo => {
                 return (
                     <div key={todo.id}>
@@ -12,7 +16,7 @@ const List = ({todos, deleteTodo}) => {
                     </div>
                 )
             })}
-        </div>
+        </VStack>
     );
 }
 
